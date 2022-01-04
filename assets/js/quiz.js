31728595737cd7b06a23e90abf9645c6626c6012
becket <becket.bowes@gmail.com>
2022-01-04 01:03:56 -0500

    basic html, css, and a pseudocoded js outline

---
+++ assets/js/quiz.js
@@ -0,0 +1,10 @@
+//start with a java generated display for welcome page and timer
+//make the timer function
+//make an array1 of array2s with each array1 being a question containing an array2 of awnsers, one of which reads false.
+//for loop the array1 through display on click
+//generate a value for the return buttons depending on the true value of the array2
+//onclick for the wrong value deducts time function, flashes error, goes to next array1
+//onclick for the right value goes to next item in array1
+//onclick to last array1 saves time as score.
+//if high score, save initials, ask to play again
+//else, return score, high score and ask to play again.
