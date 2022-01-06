96f2c8313beb2529c6ed010db6822151846f8df8
becket <becket.bowes@gmail.com>
2022-01-05 22:11:54 -0500

    page put into java

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -8,3 +8,45 @@
 //onclick to last array1 saves time as score.
 //if high score, save initials, ask to play again
 //else, return score, high score and ask to play again.
+var body = document.body;
+var header = document.createElement("header");
+var scoreTime = document.createElement("ul");
+var scoreL = document.createElement("li");
+var ul2 = document.createElement("ul");
+var timerL = document.createElement("li");
+var section = document.createElement("section");
+var quiz = document.createElement("ul");
+var questionL = document.createElement("h1");
+var answerL1 = document.createElement("li");
+var answerL2 = document.createElement("li");
+var answerL3 = document.createElement("li");
+var answerL4 = document.createElement("li");
+var answerL5 = document.createElement("li");
+var buttonL = document.createElement("h1");
+
+scoreL.textContent = "High Scores";
+timerL.textContent = "Time: 0"
+questionL.textContent = "Coding History Challenge";
+answerL1.textContent = "Try answering these questions about the history of coding within the time limit. You will be penalized 10 seconds for any wrong answers.";
+answerL2.textContent = "";
+answerL3.textContent = "";
+answerL4.textContent = "";
+answerL5.textContent = "";
+buttonL.textContent = "< Start Quiz >";
+
+body.appendChild(header);
+header.appendChild(scoreTime)
+scoreTime.appendChild(scoreL);
+scoreTime.appendChild(timerL);
+body.appendChild(section);
+section.appendChild(quiz);
+quiz.appendChild(questionL);
+quiz.appendChild(answerL1);
+quiz.appendChild(answerL2);
+quiz.appendChild(answerL3);
+quiz.appendChild(answerL4);
+quiz.appendChild(answerL5);
+quiz.appendChild(buttonL);
+
+section.setAttribute("style", "padding: 100px 30%; align-content: center;")
+buttonL.setAttribute("style", "font-weight:bold; text-decoration: underline;");
