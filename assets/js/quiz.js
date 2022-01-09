6d43a56a8c95d067723bb253eeb96af5f9ebd22a
becket <becket.bowes@gmail.com>
2022-01-08 22:54:46 -0500

    i don't even know if anything is better i just can't seem to fix the bugs

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,7 +1,7 @@
 //start with a java generated display for welcome page and timer
 var time = 120;
 var score = 0;
-var cQ = 0;
+var questionId = 0;
 var body = document.body;
 var header = document.createElement("header");
 var scoreTime = document.createElement("ul");
@@ -53,7 +53,7 @@ const poser1 = {
     a3: "1985",
     a4: "1991",
     a5: "1995",
-    key: "<li>1995<li>"
+    key: "1995"
 };
 const poser2 = {
     qq: "What was the original name of JavaScript?",
@@ -62,7 +62,7 @@ const poser2 = {
     a3: "Mocha",
     a4: "Toddy",
     a5: "Keurig",
-    key: "<li>Mocha<li>"
+    key: "Mocha",
 };
 const poser3 = {
     qq: "Who invented JavaScript?",
@@ -71,7 +71,7 @@ const poser3 = {
     a3: "Ada Lovelace",
     a4: "Brendan Eich",
     a5: "Katherine Johnson",
-    key: "<li>Brendan Eich<li>"
+    key: "Brendan Eich",
 };
 const poser4 = {
     qq: "How Long did it take Brendan Eich to write JavaScript?",
@@ -80,7 +80,7 @@ const poser4 = {
     a3: "10 weeks",
     a4: "11 weeks",
     a5: "10 months",
-    key: "<li>10 days<li>"
+    key: "10 days",
 };
 const poser5 = {
     qq: "JavaScript was written for what browser?",
@@ -89,7 +89,7 @@ const poser5 = {
     a3: "Apple Web Explorer",
     a4: "Microsoft Exporer",
     a5: "Netscape Navigator",
-    key: "<li>Netscape Navigator<li>"
+    key: "Netscape Navigator",
 };
 const poser6 = {
     qq: "Who currently maintains and develops JavaScript?",
@@ -98,25 +98,23 @@ const poser6 = {
     a3: "Mozilla",
     a4: "WikiCode",
     a5: "Linux Support Community",
-    key: "<li>Mozilla<li>"
+    key: "Mozilla",
 };
 
 //array of the quiz question objects:
 var posers = [poser1, poser2, poser3, poser4, poser5, poser6];
+var riposters = ["1995", "Mocha", "Brendan Eich", "10 days", "Netscape Navigator", "Mozilla"]
 
 //make the timer function
 var tick = function () {
-    if (time > 0) {
-        setInterval(function () {
-            time--;
-            var minute = parseInt(time / 60);
-            var second = parseInt(time % 60);
-            var clock = (minute+ ":" + second);
-            timerL.textContent = "Time Remaining: " + clock;
-        }, 1000);
-    } else {
-        clearInterval(tick);
-        timerL.textContent = "0:00";
+    setInterval(function () {
+        time--;
+        var minute = parseInt(time / 60);
+        var second = parseInt(time % 60).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false});
+        var clock = (minute + ":" + second);
+        timerL.textContent = "Time Remaining: " + clock;
+    }, 1000);
+    if (time === 0 || time < 0) { 
         endGame();
     }
 };
@@ -130,10 +128,25 @@ var tock = function () {
 var clearText = function () {
     questionL.textContent = "";
     answerL1.textContent = "";
+    answerL1.removeEventListener("click", function (event) {
+        if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+    });
     answerL2.textContent = "";
+    answerL2.removeEventListener("click", function (event) {
+        if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+    });
     answerL3.textContent = "";
+    answerL3.removeEventListener("click", function (event) {
+        if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+    });
     answerL4.textContent = "";
+    answerL4.removeEventListener("click", function (event) {
+        if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+    });
     answerL5.textContent = "";
+    answerL5.removeEventListener("click", function (event) {
+        if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+    });
     buttonL.textContent = "";
 };
 
@@ -141,83 +154,79 @@ var clearText = function () {
 
 //right answer function
 var rightO = function () {
+    //skip to next answer
+    questionId++;
+    //add to score and record it
     score++;
     scoreL.textContent = score + "/" + posers.length;
+    //tell the user they got it right
     clearText();
     questionL.textContent = "Right!";
+    //back to the puzzle
     setTimeout(() => {
-        clearInterval(rightO);
-        cQ++;
         quizzle();
     }, 500);
 };
 
 //wrong answer function
 var wrongO = function () {
+    //skip to the next answer
+    questionId++;
+    //penalize the user
     (time = time - 10);
+    //psychologically penalize the user
     clearText();
     var error = document.createElement("h2");
     error.textContent = "ERROR"
-    quiz.appendChild (error);
+    quiz.appendChild(error);
+    //back to the puzzle
     setTimeout(() => {
         error.textContent = "";
-        cQ++;
         quizzle();
     }, 500);
 };
 
-//endGame function
+//endGame function (to be elaborated with score keeping and such)
 var endGame = function () {
     clearText();
+    timerL.textContent = "Time's Up!";
     questionL.textContent = "GAME OVER"
 };
 
 //question function
 var quizzle = function () {
-    //call question object from array
-    var thisQuestion = posers[cQ];
-    //reformat text on page to question/key 
-    questionL.textContent = thisQuestion.qq;
-    console.log(thisQuestion.qq)
-    questionL.setAttribute = "#" + thisQuestion.key;
-    console.log(thisQuestion.key);
-    //...and awnser buttons
-    answerL1.textContent = thisQuestion.a1;
-    answerL1.addEventListener("click", function (event) {
-        if (event.target === thisQuestion.key) { rightO(); } else { wrongO(); };
-        console.log(event.target);
-        console.log(thisQuestion);
-    });
-    answerL2.textContent = thisQuestion.a2;
-    answerL2.addEventListener("click", function (event) {
-        if (event.target === thisQuestion.key) { rightO(); } else { wrongO(); };
-        console.log(event.target);
-        console.log(thisQuestion);
-    });
-    answerL3.textContent = thisQuestion.a3;
-    answerL3.addEventListener("click", function (event) {
-        if (event.target === thisQuestion.key) { rightO(); } else { wrongO(); };
-        console.log(event.target);
-        console.log(thisQuestion);
-    });
-    answerL4.textContent = thisQuestion.a4;
-    answerL4.addEventListener("click", function (event) {
-        if (event.target === thisQuestion.key) { rightO(); } else { wrongO(); };
-        console.log(event.target);
-        console.log(thisQuestion);
-    });
-    answerL5.textContent = thisQuestion.a5;
-    answerL5.addEventListener("click", function (event) {
-        if (event.target === thisQuestion.key) { rightO(); } else { wrongO(); };
-        console.log(event.target);
-        console.log(thisQuestion);
-    });
+    if (time < 0 || questionId > posers.length) {
+        endGame();
+    } else {
+        //call question object from array
+        var thisQuestion = posers[questionId];
+        //reformat text on page to question
+        questionL.textContent = thisQuestion.qq;
+        //...and awnser buttons
+        answerL1.textContent = thisQuestion.a1;
+        answerL1.addEventListener("click", function (event) {
+            if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+        });
+        answerL2.textContent = thisQuestion.a2;
+        answerL2.addEventListener("click", function (event) {
+            if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+        });
+        answerL3.textContent = thisQuestion.a3;
+        answerL3.addEventListener("click", function (event) {
+            if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+        });
+        answerL4.textContent = thisQuestion.a4;
+        answerL4.addEventListener("click", function (event) {
+            if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+        });
+        answerL5.textContent = thisQuestion.a5;
+        answerL5.addEventListener("click", function (event) {
+            console.log(event.target.textContent);
+            if (event.target.textContent === riposters[questionId]) { rightO(); } else { wrongO(); };
+        });
+    }
 };
 
-// if (time = 0 || -1) {endGame()};
-
-
-// var endGame = function() {
 // record score
 // show end screen    
 // };
@@ -229,8 +238,9 @@ var startTest = function (event) {
     clearText();
     quizzle();
     setTimeout(() => {
+        clearInterval(tick);
         endGame();
-    }, 100000);
+    }, parseInt(time * 1000));
 };
 
 buttonL.addEventListener("click", startTest);
